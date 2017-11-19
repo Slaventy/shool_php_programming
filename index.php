719ff93f6eb3e027fef98cf832dd9a3b1321e3fb
Viacheslav Lebedev <viacheslavCreator@yandex.ru>
2017-11-19 17:08:49 +0300

    урок4

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+?>
+<!DOCTYPE HTML PUBLIC "">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Title</title>
+        <meta name="description" content="Уроки" >
+        <meta name="keywords" content="Уроки">
+        <link href="st.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+    <div id="maincontent">
+	    <!--
+        <a href ="#">https://www.templatemonster.com/ru/demo/42889.html"</a>
+        <a href="#">https://www.templatemonster.com/ru/demo/42835.html"</a>
+
+        <p class="newclass">https://www.templatemonster.com/ru/demo/42889.html</p>
+        <p class="newclass2">https://www.templatemonster.com/ru/demo/42835.html</p> -->
+	    <div class="block1">
+	        <p>Текст который мы пишем большойдлинныйтекст</p>
+		    <div>простой текст</div>
+	        <p>Текст который мы пишем большойдлинныйтекст</p>
+	        <p>Текст который мы пишем большойдлинныйтекст</p>
+        </div>
+	    <div class="block2">
+	        <p>Текст который мы пишем большойдлинныйтекст</p>
+		    <div>простой текст</div>
+	        <p>Текст который мы пишем большойдлинныйтекст</p>
+	        <p>Текст который мы пишем большойдлинныйтекст</p>
+        </div>
+    </div>
+    </body>
+</html>
